232eb5c9bb6197d57c8e3539edfad0d4b5df36c2
John Fajardo <fajardoc.j@gmail.com>
2019-08-26 14:45:04 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -23,13 +23,32 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+
+
+   handleMouseMove = (event) => {
+     drawChromeBoiAtCoords(event.clientX, event.clientY); 
+   }
+
+
+   handleKey = (event) => {
+     if(event.key === 'a'){
+       resize('+');
+     } else if(event.key === 's'){
+       resize('-');
+     }
+   }
+
+
+
   
   render() {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={() => {toggleCycling()}}
+        onKeyDown={this.handleKey}
         width='900'
-        height='600'
+        height='600' 
         tabIndex="0">
       </canvas>
     )
